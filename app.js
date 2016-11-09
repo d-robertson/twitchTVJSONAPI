d2a4bd2023b145c3ad4a06b0f97f6ff6aa8173be
derek_robertson <dwilliamrobertson@gmail.com>
2016-11-09 15:36:32 -0800

    added recursion for making multiple http calls

--- app.js
+++ app.js
@@ -8,29 +8,68 @@ angular.module('twitchApp', [])
 
 .controller('twitchCtrl', ['$scope', '$http', function($scope, $http){
 
-  var options = {
-    width: 640,
-    heigth: 360,
-    channel: 'freecodecamp'
-  }
+  // var options = {
+  //   width: 640,
+  //   heigth: 360,
+  //   channel: 'freecodecamp'
+  // }
 
-  var Player = new Twitch.Player('player1', options);
-  player.setVolume(0.5);
+  // var Player = new Twitch.Player('player1', options);
+  // player.setVolume(0.5);
+  $scope.streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+  $scope.fullStreams = [];
+
+  $scope.counter = 0;
+
+  $scope.addSearch = function(){
+    $scope.streams.push($scope.search);
+    $scope.search = '';
+  }
 
   $scope.twitchReq = function(){
-
-    var req = {
-      url: 'https://api.twitch.tv/kraken/streams/freecodecamp',
-      params: {
-        client_id: 'gh4p8qnbrzxx1au8hnphujd0n5n34b5'
+    if($scope.streams.length - 1 >= $scope.counter){
+      var req = {
+        url: 'https://api.twitch.tv/kraken/streams/' + $scope.streams[$scope.counter],
+        params: {
+          client_id: 'gh4p8qnbrzxx1au8hnphujd0n5n34b5'
+        }
       }
-    }
+       var title = $scope.streams[$scope.counter];
+       console.log('set: ',title);
+      // console.log(req);
 
-    $http(req).then(function success(res){
-      console.log('success: ', res.data);
-    }, function error(res){
-      console.log('error: ', res);
-    }) 
+      $http(req).then(function success(res){
+
+        // console.log('success: ', res.data);
+        console.log('in http: ', title);
+        var status = '';
+
+        if(res.data.stream === null){
+          status = 'Offline';
+        } else {
+          status = 'Online';
+        }
+
+        var stream = {
+          streamTitle: title,
+          status: status,
+          data: res.data
+        }
+
+        console.log('streamObj: ', stream);
+
+        $scope.fullStreams.push(stream);
+        $scope.counter += 1;
+        console.log($scope.counter);
+        $scope.twitchReq();
+         
+      }, function error(res){
+
+        console.log('error: ', res);
+
+      }); 
+    }
+    console.log('fullStreams: ', fullStreams);
   }
-  
 }]);
